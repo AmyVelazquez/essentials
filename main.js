ce86fc80aeaea7e52257d44fdaf56acb040eec00
Anna Gondring <32964281+zednon@users.noreply.github.com>
2023-11-17 09:51:15 -0500

    changes to CSS

--- main.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const mobileMenu = document.getElementById('mobile-menu');
-    const nav = document.querySelector('.nav');
-  
-    mobileMenu.addEventListener('click', function () {
-      nav.classList.toggle('show');
-    });
-  });
